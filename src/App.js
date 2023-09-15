44090902b70549edc20bc65a10836e6e54f32d40
Natalia Kur <natalia.kur@frontkom.com>
2023-09-15 15:43:57 +0200

    Initial config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit
+        <code>src/App.js</code>
+        and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+        alt="roman"
+      >
+        ecimen book. It has survived not only five centuries, but hhhhhalso the leap into
+        electronichfghfhjfhfffhgfhghfhfghfhfhg typesetting, remaining essentially unchanged. It was
+        popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+        passages, and more recently with desktop publishing software like Aldus PageM
+      </a>
+    </header>
+  </div>;
+};
 
 export default App;
